8aca25b63bb15c6cac53d2bc74e980cdfa8e1250
Jose <suportejr@outlook.com>
2024-11-16 17:52:51 -0300

    feat: Criando gerador de senha default criptografada

---
+++ src/main/java/com/viannarp/consultamedica/security/PasswordHasher.java
@@ -0,0 +1,12 @@
+package com.viannarp.consultamedica.security;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class PasswordHasher {
+	public static void main(String[] args) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        String rawPassword = "admin@123";
+        String hashedPassword = encoder.encode(rawPassword);
+        System.out.println("Hashed Password: " + hashedPassword);
+    }
+}
